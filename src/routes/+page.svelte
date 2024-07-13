fa67da22b342b7a44f421e9672440c7b364b5667
Simeon Duwel <simeon.duwel@student.kuleuven.be>
2024-07-13 03:13:27 +0200

    initiële commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="w-screen h-screen absolute insert-0 p-6 flex flex-col justify-stretch items-center bg-red-100">
+    <h1 class="text-4xl font-bold">Echt Actief Yaplandia</h1>
+    <p>Voor studenten die zowel echt, actief als yappende zijn</p>
+    <p>Meer info volgt!</p>
+</div>
