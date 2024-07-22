7863fb840da1e53ea205ec3e6dab2f2ab846ac16
Simeon Duwel <simeon.duwel@student.kuleuven.be>
2024-07-22 09:51:42 +0200

    statuten toegevoegd

---
+++ src/routes/statuten/+page.svelte
@@ -0,0 +1,210 @@
+<h2 class="text-3xl">Statuten</h2>
+Hieronder vind je de statuten van Echt Actief Yaplandia.
+
+<div class="flex flex-col p-4 rounded-md bg-white shadow-md">
+    <h3 class="text-xl mb-4 font-bold">ALGEMEEN</h3>
+
+    <p>
+        0. Gezien deze vereniging geen echte vzw is zijn deze statuten niet
+        bindend en puur voor de leut. Vanaf een vzw wordt opgericht met deze
+        naam vervalt dit artikel en worden de andere bindend.
+    </p>
+    <p>
+        1. De vereniging is opgericht op 12 juli 2024 te Bonheiden, en draagt de
+        naam "Echt Actief Yaplandia".
+    </p>
+    <p>
+        2. Echt Actief Yaplandia is opgericht door de volgende personen,
+        opgelijst in alfabetische volgorde, hierna de "stichters":
+    </p>
+    <ul class="list-disc list-inside">
+        <li>Andries De Klerck</li>
+        <li>Free Staquet</li>
+        <li>Hardwin Voorspoels</li>
+        <li>Joakim Van Dooren Landuyt</li>
+        <li>Niels Libeer</li>
+        <li>Matt Briers</li>
+        <li>Simeon Duwel</li>
+    </ul>
+    <p>
+        3. Het doel van Echt Actief Yaplandia is het promoten en vieren van de
+        kunst van het yappen, en elk evenement dat Echt Actief Yaplandia
+        organiseert dient dit doel te steunen.
+    </p>
+    <p>
+        4. De kleuren van Echt Actief Yaplandia zijn zwart op wit. Andere
+        kleuren zijn toegelaten waar nodig.
+    </p>
+
+    <h3 class="text-xl mb-4 mt-2 font-bold">SAMENSTELLING</h3>
+    <p>
+        5. Echt Actief Yaplandia heeft verschillende leden, onderverdeeld in
+        Yappers, Actief Yappers en Echt Actief Yappers.
+    </p>
+    <p>
+        6. Yappers zijn individuen die een lidkaart gekocht hebben en succesvol
+        een doop hebben voltooid.
+    </p>
+    <p>
+        7. Een lidkaart mag niet meer kosten dan 9 Chicken McNuggets in de
+        McDonalds gevestigd te Kortestraat 7/9, 3000 Leuven. Deze is geldig
+        vanaf moment van aankoop tot de start van het volgende academiejaar aan
+        de KU Leuven.
+    </p>
+    <p>
+        8. Actief Yappers zijn Yappers die lid zijn van de ploeg, en in deze
+        functie taken opnemen die Echt Actief Yaplandia ten goede komen.
+    </p>
+    <p>9. Echt Actief Yappers zijn bestuursleden van deze vzw.</p>
+    <p>
+        10. Indien een van de stichters niet onder Echt Actief Yapper valt, dan
+        geniet deze van alle rechten maar geen van de plichten van Echt Actief
+        Yappers. Indien een van de stichters niet onder Actief Yapper valt, dan
+        geniet deze van alle rechten maar geen van de plichten van Actief
+        Yappers. Indien een van de stichters niet onder Yapper valt, dienen alle
+        Actieve Yappers dagelijks een bericht te sturen via een medium naar
+        keuze om de stichter in kwestie hier op te wijzen. Dit dient door te
+        gaan tot de stichter weer een Yapper is.
+    </p>
+    <p>
+        11. Vermelding van het verboden woord uit Lorem Ipsum leidt tot
+        onmiddelijke uitsluiting van de vereniging
+    </p>
+    <p>
+        11bis. Het verboden woord in bovenstaand artikel is 4 letters lang en
+        rijmt op hesse.
+    </p>
+
+    <h3 class="text-xl mb-4 mt-2 font-bold">VERGADERINGEN</h3>
+    <p>12. Een vergadering van Echt Actief Yaplandia is officieel indien:</p>
+    <ul class="list-disc list-inside">
+        <li>Er zijn minstens 2 Actief Yappers aanwezig</li>
+        <li>Er wordt gestemd dat de vergadering officieel is</li>
+        <li>
+            Een verslag wordt gemaakt dat op een volgende vergadering kan worden
+            goedgekeurd.
+        </li>
+    </ul>
+    <p>
+        13. De yap-actis houdt een geordende lijst bij van wie er wanneer
+        binnenkomt en naar alle aanwezigen wordt dan verwezen als "Voor" +
+        index. Deze index is zero-based, want dat is de superieure manier van
+        indexatie.
+    </p>
+
+    <h3 class="text-xl mb-4 mt-2 font-bold">VERPLICHTINGEN</h3>
+    <p>
+        14. Echt Actief Yaplandia verplicht zich er toe om alle verslagen van
+        Echt Actief Yaplandia vergaderingen publiek toegankelijk te maken binnen
+        3 maand van hun goedkeuring.
+    </p>
+    <p>
+        15. Zolang de statuten van Scientica vzw niet verplichten dat Scientica
+        vzw de verslagen van hun vergaderingen publiek toegankelijk maakt,
+        verplicht Echt Actief Yaplandia zich er toe om dit te doen voor hun.
+    </p>
+    <p>
+        16. Indien een andere vereniging dezelfde stunt pullt, moet op de
+        eerstvolgende vergadering van Echt Actief Yaplandia besproken worden of
+        dezelfde maatregel getroffen moet worden voor deze vereniging.
+    </p>
+    <p>
+        17. Elke Yapper wordt verwacht het goede woord van de kunst van het
+        yappen te verspreiden. De manier waarop deze taak wordt voltooid mag
+        individueel gekozen worden door elke Yapper.
+    </p>
+    <p>
+        18. Hoewel yappen inherent onlogisch is, is dit niet inherent
+        respectloos. Elke Yapper wordt verwacht ten alle tijden respectvol te
+        blijven tijdens het yappen.
+    </p>
+    <p>
+        19. Alle lessen, oefenzittingen en andere al dan niet officiëel
+        ingeplande bezigheden binnen een opleiding aan de KUL hoeven niet
+        bijgewoond te worden indien deze samenvallen met een vergadering of
+        activiteit van Echt Actief Yaplandia.
+    </p>
+    <p>
+        19bis. Bezigheden met verplichte aanwezigheid, zoals examens, vallen
+        hier niet onder.
+    </p>
+
+    <h3 class="text-xl mb-4 mt-2 font-bold">VERBANDEN MET ANDERE VERENIGINGEN EN INDIVIDUEN</h3>
+    <p>
+        20. Echt Actief Yaplandia verwerpt expliciet alle banden met Martin
+        Heidegger en andere nazi's.
+    </p>
+    <p>
+        21. Echt Actief Yaplandia staat toe dat iedereen Yapper wordt, buiten
+        individuen die deel uitmaken van Yaplandia.
+    </p>
+    <p>
+        22. Met uitzondering van Art. 21 erkent Echt Actief Yaplandia geen
+        vereniging onder de naam Yaplandia.
+    </p>
+    <p>
+        23. "Bevriende kringen" zijn alle kringen waar een Actief Yapper in het
+        bestuur/kern zit of heeft gezeten, met uitzondering van kringen waarvan
+        de gangbare naam een afkorting met enkel medeklinkers is.
+    </p>
+
+    <h3 class="text-xl mb-4 mt-2 font-bold">ACTIVITEITEN</h3>
+    <p>
+        24. Echt Actief Yaplandia heeft als hoofddoel het goede woord van de
+        kunst van het yappen te verspreiden. In functie hiervan (en enkel
+        hiervan) mogen activiteiten georganiseerd worden.
+    </p>
+    <p>
+        24bis. Wat al dan niet valt onder "het goede woord van de kunst van het
+        yappen verspreiden" valt mag ingevuld worden door elke Actieve Yapper.
+    </p>
+    <p>
+        24ter. Het goede woord van de kunst van het yappen kan ook verspreid
+        worden in kringen waar enkel Yappers aanwezig zijn. Interne activiteiten
+        zijn dus toegelaten.
+    </p>
+    <p>
+        25. Op elke activiteit georganiseerd door Echt Actief Yaplandia moet een
+        unhinged rant van minstens vijf (5) minuten plaatsvinden. Voorbeelden
+        van onderwerpen zijn conspiracy theories, de structuur van kerstbomen,
+        een (poging tot) het verdedigen van de KU Leuven Authenticator app, en
+        elk gesprek over Middle Earth waar diepgaande kennis van de Silmarillon
+        voor vereist is.
+    </p>
+    <p>
+        26. Kruiwagens met meer dan 2 wielen, parachutes, auto's geproduceerd
+        voor 1987, halogeenlampen en afbeeldingen van Chinese dictators zijn
+        verboden op elke activiteit, inclusief evenementen waar Tom Van Grieken
+        aanwezig is.
+    </p>
+    <p>27. imagine the cacao</p>
+    <p>
+        28. Met uitzondering van interne evenementen is iedereen die afkomt
+        welkom op elke activiteit van Echt Actief Yaplandia. Mensen die niet
+        naar de activiteit komen zijn niet welkom.
+    </p>
+    <p>
+        29. Bij elke activiteit waar tafels aanwezig zijn, dienen er ook boeken
+        kaarten te zijn zodat er gewiest kan worden.
+    </p>
+    <p>
+        30. Indien een individu een vleugelpiano van minstens 3.600.000 euro
+        meeneemt naar een activiteit, maar niet het clublied kan begeleiden
+        hierop, moet het individu in kwestie onmiddellijk de activiteit verlaten
+        of de vleugelpiano schenken aan Echt Actief Yaplandia.
+    </p>
+    <p>
+        31. Inkomprijzen moeten een even aantal euros zijn. Hoeveel cent
+        gevraagd wordt mag elk natuurlijk getal in het interval [0, 99] zijn,
+        zolang het geen priemgetal is.
+    </p>
+    <p>
+        32. Iedereen ouder dan 80 krijgt 50% korting op alles, indien beide
+        ouders ook aanwezig zijn op de activiteit.
+    </p>
+    <p>
+        33. Op cantussen mogen kringliederen enkel gezongen worden indien de
+        kring een bevriende kring is, tenzij de aanvrager geen lid is van de
+        kring in kwestie en de senior het ludiek genoeg vindt.
+    </p>
+</div>
