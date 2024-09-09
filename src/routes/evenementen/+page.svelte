69f9d5026dfd49494b60540825d1e1838bfd46b1
Simeon Duwel <simeon.duwel@student.kuleuven.be>
2024-09-09 12:35:49 +0200

    vergane evenementen verkleind

--- src/routes/evenementen/+page.svelte
+++ src/routes/evenementen/+page.svelte
@@ -35,21 +35,25 @@
 {#if data.afgelopenEvenementen && data.afgelopenEvenementen.length}
     Hieronder vind je evenementen die afgelopen zijn.
 
-    <div class="grid lg:grid-cols-2 gap-4 w-full max-w-2/3">
+    <div class="grid lg:grid-cols-3 gap-4 w-full max-w-2/3">
         {#each data.afgelopenEvenementen as evenement}
             <a
-                class="bg-white rounded-md p-4 flex flex-col gap-4"
+                class="bg-white rounded-md p-4 flex flex-row items-center gap-4"
                 href={"evenementen/" + evenement.slug}
             >
-                <h3 class="text-xl">{evenement.naam}</h3>
-                <p>{evenement.start.toLocaleString("en-GB")}</p>
                 <img
+                    class="aspect-square w-16"
                     src={imageBuilder
                         .image(evenement.fotoAsset)
-                        .width(400)
+                        .width(200)
+                        .height(200)
                         .url()}
                     alt={evenement.naam}
                 />
+                <div class="flex flex-col">
+                    <h3 class="text-xl">{evenement.naam}</h3>
+                    <p class="text-neutral-600">{new Date(evenement.start).toLocaleDateString("nl-BE")}</p>
+                </div>
             </a>
         {/each}
     </div>
